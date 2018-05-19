66109053866851dbfd0d8e8beb1ca56dc2efc1fe
Tom Cupis <tcupis250@gmail.com>
2018-05-19 15:26:34 +0100

    auto window centering added
    Python3 supported,
    Window centering added

--- gui.py
+++ gui.py
@@ -5,40 +5,45 @@ class App():
     WIDTH = 1100 # > 8*TILE_SIZE
     HEIGHT = 0 # For auto height from width, keep small
     TILE_SIZE = 125 # Default=125
+    
     def __init__(self):
         self.fixSize()
         self.createGui()
-        
+
     def fixSize(self):
         spacingX = (self.WIDTH-(self.TILE_SIZE*8))/9.0
         spacingY = (self.HEIGHT-(self.TILE_SIZE*8))/9.0
         if ((spacingX > spacingY)and(spacingY>0)): # Height restrict
             self.SPACING = spacingY
             self.WIDTH = int(0.5+(7*self.SPACING)+(5*self.TILE_SIZE))
-            print "Resizing width", self.WIDTH, self.SPACING
+            print("Resizing width", self.WIDTH, self.SPACING)
         else: # Width restrict
             self.SPACING = spacingX
-            print self.HEIGHT,
+            print(self.HEIGHT,)
             self.HEIGHT = int(0.5+(7*self.SPACING)+(6*self.TILE_SIZE))
-            print "Resizing height", self.HEIGHT, self.SPACING
-            
+            print("Resizing height", self.HEIGHT, self.SPACING)
+
     def createGui(self):
         self.root = Tk.Tk()
         self.root.title("Forbidden Island")
-        self.root.geometry("{0}x{1}".format(self.WIDTH, self.HEIGHT))
+
+        self.SCREEN_WIDTH = int(self.root.winfo_screenwidth())
+        self.SCREEN_HEIGHT = int(self.root.winfo_screenheight())
+        self.root.geometry("{0}x{1}+{2}+{3}".format(self.WIDTH, self.HEIGHT, (self.SCREEN_WIDTH-self.WIDTH)//2, (self.SCREEN_HEIGHT-self.HEIGHT)//2))
         self.root.resizable(0,0)
+
         self.canvas = Tk.Canvas(self.root, width=self.WIDTH, height=self.HEIGHT, bg="#705840")
         self.canvas.pack()
     mainloop = lambda self: self.root.mainloop()
-    
+
     def drawRect(self, x1,y1, x2,y2):
         self.canvas.create_rectangle(x1,y1, x2,y2, fill="DARK GREY")
-        
+
     def addTile(self, x, y):
         xPos = self.TILE_SIZE*x + self.SPACING*(x+1)
         yPos = self.TILE_SIZE*y + self.SPACING*(y+1)
         self.drawRect(xPos,yPos, xPos+self.TILE_SIZE, yPos+self.TILE_SIZE)
-    
+
     def addTiles(self):
         spacing = self.SPACING
         for y in range(6):
@@ -49,8 +54,6 @@ class App():
                 #xPos = self.TILE_SIZE*(x+1+xOffset) + spacing*(x+2+xOffset)
                 self.addTile(x+xOffset, y)
                 #self.drawRect(xPos,yPos, xPos+self.TILE_SIZE, yPos+self.TILE_SIZE)
-            
-        
 
 root = App()
 root.addTiles()
