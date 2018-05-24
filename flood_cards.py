72828f70ea68db4f44c9107e18683da41e59fdb2
Tom Cupis <tcupis250@gmail.com>
2018-05-24 16:56:21 +0100

    Added initial sinking

--- flood_cards.py
+++ flood_cards.py
@@ -1,15 +1,30 @@
 import random
+
+#Flood cards
 class FloodCard():
 
     FLOOD_CARDS_PATH = "flood_cards.csv"
 
     def __init__(self):
+        "-> void"
+
         self.flood_cards = []
-        for i in list(open(self.FLOOD_CARDS_PATH, 'r').readlines()): self.flood_cards.append(i.replace("\n", ""))
-        print(flood_cards)
+
+        for i in list(open(self.FLOOD_CARDS_PATH, 'r').readlines()):
+            self.flood_cards.append(i.replace("\n", "").split(","))
+
 
     def initial_sinking(self):
-        "To do"
+        "-> void"
+
+        temp_cards = self.flood_cards
+        random.shuffle(temp_cards)
+        selected_cards = temp_cards[0:6] #select top 6 shuffled cards
+
+        print(selected_cards)
 
 
 app = FloodCard()
+
+for i in range(100):
+    app.initial_sinking()
